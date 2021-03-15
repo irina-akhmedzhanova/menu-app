46462608519c7805c4123a36c6d7afa3d67320cb
Irina <akhmedzhanova.irina523400@gmail.com>
2021-03-15 16:55:54 +0500

    create current menu page and add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { MenuPage } from './pages/MenuPage';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { Layout, Menu } from 'antd';
+import { MenuPage } from './pages/MenuPage/MenuPage';
+import classes from './App.module.scss';
 
 function App() {
+  const { Header, Content, Footer } = Layout;
+
   return (
-    <MenuPage />
+    <Provider store={store}>
+      <Layout className={classes.layout}>
+        <Header>
+          <Menu theme="dark" mode="horizontal"></Menu>
+        </Header>
+        <Content style={{ padding: '10px 50px' }}>
+          <MenuPage />
+        </Content>
+        <Footer style={{textAlign: 'center'}}>Created by Us</Footer>
+      </Layout>
+    </Provider>
   );
 }
 
